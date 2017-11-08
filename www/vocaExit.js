c7aa8213003c129185bec3f3e89ac7a03665f540
crossee <a01064297102@gmail.com>
2017-11-08 11:55:52 +0900

    no message

--- www/vocaExit.js
+++ www/vocaExit.js
@@ -1,23 +1,24 @@
 var argscheck = require('cordova/argscheck'),
-   utils = require('cordova/utils'),
-   exec = require('cordova/exec');
+    utils = require('cordova/utils'),
+    exec = require('cordova/exec');
 
-   window.navigator.app.exitApp = function(askUser){
-     if(!askUser) {
-       askUser = false;
-     }
+    window.navigator.app = {
+      exitApp:function(askUser){
+        if(!askUser) {
+          askUser = false;
+        }
 
-     exec(
-       function callback(data) {
-         console.log("callback Message");
-       },
-       function errorHandler(err) {
-         console.log("Error" + err);
-       },
-       'VocaExit',
-       'exitApp',
-       [askUser]
-     );
-   };
-
-   module.exports = window.navigator.app.exitApp;
+        exec(
+          function callback(data) {
+            console.log("callback Message");
+          },
+          function errorHandler(err) {
+            console.log("Error" + err);
+          },
+          'VocaExit',
+          'exitApp',
+          [askUser]
+        );
+      }
+    };
+    module.exports = navigator.app;
